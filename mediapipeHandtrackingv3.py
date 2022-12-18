3efb2c12bca4fedca64aad3a72fb6e5d7427e68c
bartek <bartekpiatek123@gmail.com>
2022-12-18 01:24:25 +0100

    first working xyz, tested with delta

--- mediapipeHandtrackingv3.py
+++ mediapipeHandtrackingv3.py
@@ -6,14 +6,21 @@ import numpy as np
 import argparse
 import utils.anchors
 import utils.processing as mpu
+import threading
+import socket
+from time import sleep
+from math import sqrt
 
 FINGER_COLOR = [(128, 128, 128), (80, 190, 168),
-         (234, 187, 105), (175, 119, 212),
-         (81, 110, 221)]
+                (234, 187, 105), (175, 119, 212),
+                (81, 110, 221)]
 
 JOINT_COLOR = [(0, 0, 0), (125, 255, 79),
-            (255, 102, 0), (181, 70, 255),
-            (13, 63, 255)]
+               (255, 102, 0), (181, 70, 255),
+               (13, 63, 255)]
+
+hand_pos = ()
+points_xyz = ()
 
 # depth var
 config = None
@@ -21,28 +28,31 @@ newConfig = False
 topLeft = dai.Point2f(0.4, 0.4)
 bottomRight = dai.Point2f(0.6, 0.6)
 stepSize = 0.05
-points_storage = []
+points_storage = {}
+
 
 # def to_planar(arr: np.ndarray, shape: tuple) -> list:
 def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
-    resized = cv2.resize(arr, shape, interpolation=cv2.INTER_NEAREST).transpose(2,0,1)
+    resized = cv2.resize(arr, shape, interpolation=cv2.INTER_NEAREST).transpose(2, 0, 1)
     return resized
+
 
 pipeline = dai.Pipeline()
 
 SSDAnchorOptions = namedtuple('SSDAnchorOptions', [
-        'num_layers',
-        'min_scale',
-        'max_scale',
-        'input_size_height',
-        'input_size_width',
-        'anchor_offset_x',
-        'anchor_offset_y',
-        'strides',
-        'aspect_ratios',
-        'reduce_boxes_in_lowest_layer',
-        'interpolated_scale_aspect_ratio',
-        'fixed_anchor_size'])
+    'num_layers',
+    'min_scale',
+    'max_scale',
+    'input_size_height',
+    'input_size_width',
+    'anchor_offset_x',
+    'anchor_offset_y',
+    'strides',
+    'aspect_ratios',
+    'reduce_boxes_in_lowest_layer',
+    'interpolated_scale_aspect_ratio',
+    'fixed_anchor_size'])
+
 
 class HandTracker:
     def __init__(self,
@@ -284,12 +294,11 @@ class HandTracker:
 
             hand_bbox = [draw_min_x, draw_min_y, draw_max_x, draw_max_y]
 
-
             if self.show_hand_box:
 
                 cv2.rectangle(frame, (draw_min_x, draw_min_y), (draw_max_x, draw_max_y), (36, 152, 0), 2)
                 cv2.putText(frame, f"({int(x_center)}, {int(y_center)})", (int(x_center), int(y_center)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
                 global points_storage
                 # def mapping_val(old_val, old_min, old_max, new_min, new_max):
                 #     old_range = (old_max - old_min)
@@ -300,12 +309,27 @@ class HandTracker:
                 #                       bottomRight=(mapping_val(draw_max_x, 0, 576, 0.05, 0.95),
                 #                                mapping_val(draw_min_y, 0, 324, 0.05, 0.95)))
                 # map values to
-                draw_min_x = draw_min_x/576
-                draw_max_y = draw_max_y/324
-                draw_max_x = draw_max_x/576
-                draw_min_y = draw_min_y/324
+                draw_min_x = draw_min_x / 576
+                draw_max_y = abs(draw_max_y - 2*draw_size) / 324
+                draw_max_x = draw_max_x / 576
+                draw_min_y = abs(draw_min_y-2*draw_size) / 324
                 points_storage = dict(topLeft=(draw_min_x, draw_max_y),
                                       bottomRight=(draw_max_x, draw_min_y))
+
+                print(draw_min_x, draw_min_y, draw_max_x, draw_max_y)
+
+                global hand_pos
+                x = x_center / 576
+                y = y_center / 324
+                if x >= 1:
+                    x = 0.99
+                elif x <= 0:
+                    x = 0.01
+                if y >= 1:
+                    y = 0.99
+                elif y <= 0:
+                    y = 0.01
+                hand_pos = (round(x, 1), round(y, 1))
 
         return cropped_frame, region.handedness, hand_bbox
 
@@ -359,7 +383,6 @@ class HandTracker:
                 self.lm_postprocess(r, inference)
                 hand_frame, handedness, hand_bbox = self.lm_render(video_frame, annotated_frame, r)
 
-
             # depthmap
             inDepth = q_depth.get()  # Blocking call, will wait until a new data has arrived
 
@@ -382,14 +405,17 @@ class HandTracker:
                 depthMax = depthData.depthMax
 
                 fontType = cv2.FONT_HERSHEY_TRIPLEX
-                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), (255,255,255),
+                global points_xyz
+                points_xyz = (int(depthData.spatialCoordinates.x), int(depthData.spatialCoordinates.y),
+                              int(depthData.spatialCoordinates.z))
+                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), (255, 255, 255),
                               cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
                 cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20),
-                            fontType, 0.5, (255, 255, 255))
+                            fontType, 0.5, (0, 255, 0))
                 cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35),
-                            fontType, 0.5, (255, 255, 255))
+                            fontType, 0.5, (0, 255, 0))
                 cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50),
-                            fontType, 0.5, (255, 255, 255))
+                            fontType, 0.5, (0, 255, 0))
             # Show the frame
             cv2.imshow("depth", depthFrameColor)
 
@@ -409,26 +435,26 @@ class HandTracker:
                 self.show_hand_box = not self.show_hand_box
             elif key == ord('2'):
                 self.show_landmarks = not self.show_landmarks
-            # elif key == ord('w'):
-            #     if topLeft.y - stepSize >= 0:
-            #         topLeft.y -= stepSize
-            #         bottomRight.y -= stepSize
-            #         newConfig = True
-            # elif key == ord('a'):
-            #     if topLeft.x - stepSize >= 0:
-            #         topLeft.x -= stepSize
-            #         bottomRight.x -= stepSize
-            #         newConfig = True
-            # elif key == ord('s'):
-            #     if bottomRight.y + stepSize <= 1:
-            #         topLeft.y += stepSize
-            #         bottomRight.y += stepSize
-            #         newConfig = True
-            # elif key == ord('d'):
-            #     if bottomRight.x + stepSize <= 1:
-            #         topLeft.x += stepSize
-            #         bottomRight.x += stepSize
-            #         newConfig = True
+            elif key == ord('w'):
+                if topLeft.y - stepSize >= 0:
+                    topLeft.y -= stepSize
+                    bottomRight.y -= stepSize
+                    newConfig = True
+            elif key == ord('a'):
+                if topLeft.x - stepSize >= 0:
+                    topLeft.x -= stepSize
+                    bottomRight.x -= stepSize
+                    newConfig = True
+            elif key == ord('s'):
+                if bottomRight.y + stepSize <= 1:
+                    topLeft.y += stepSize
+                    bottomRight.y += stepSize
+                    newConfig = True
+            elif key == ord('d'):
+                if bottomRight.x + stepSize <= 1:
+                    topLeft.x += stepSize
+                    bottomRight.x += stepSize
+                    newConfig = True
 
             if points_storage:
                 # xdiff = abs(bottomRight.x - topLeft.x)
@@ -440,7 +466,7 @@ class HandTracker:
                 topLeft = dai.Point2f(points_storage["topLeft"][0], points_storage["topLeft"][1])
                 bottomRight = dai.Point2f(points_storage["bottomRight"][0],points_storage["bottomRight"][1])
                 newConfig = True
-                print(points_storage)
+            #     print(points_storage)
             # print(topLeft.x, " ", topLeft.y)
             # print(bottomRight.x, " ", bottomRight.y)
 
@@ -452,14 +478,167 @@ class HandTracker:
                 q_spatial_calc_conf.send(cfg)
                 newConfig = False
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--pd_m", default="models/palm_detection_6_shaves.blob", type=str,
-                        help="Path to a blob file for palm detection model (default=%(default)s)")
-    parser.add_argument("--lm_m", default="models/hand_landmark_6_shaves.blob", type=str,
-                        help="Path to a blob file for landmark model (default=%(default)s)")
-    args = parser.parse_args()
 
-    ht = HandTracker(pd_path=args.pd_m, lm_path=args.lm_m)
-    ht.run()
+def delta_loop():
+    global hand_pos
+    i = 0
+    while True:
+        if hand_pos:
 
+            # x_norm_int = hand_pos[0] * 6000 - 3000
+            # y_norm_int = hand_pos[1] * 6000 - 3000
+            x_norm_int = points_xyz[0]
+            y_norm_int = points_xyz[0]
+            z_norm_int = -abs(points_xyz[0]) - 5000
+            if z_norm_int > -4000:
+                z_norm_int = -4000
+            elif z_norm_int < -7000:
+                z_norm_int = -7000
+            if x_norm_int >= 0:
+                x_norm = "-" + str(int(abs(hand_pos[0] * 6000 - 3000))).zfill(4)
+            else:
+                x_norm = "+" + str(abs(int(hand_pos[0] * 6000 - 3000))).zfill(4)
+
+            if y_norm_int >= 0:
+                y_norm = "-" + str(int(hand_pos[1] * 6000 - 3000)).zfill(4)
+            else:
+                y_norm = "+" + str(int(hand_pos[1] * 6000 - 3000)).zfill(4)
+
+            command = f"JNT{x_norm}{y_norm}-{str(abs(z_norm_int))}TOOL_V0010"
+            # print(points_xyz)
+            print(command)
+
+            if i == 0:
+                delta_sock.send(command.encode())
+                prev_x = x_norm_int
+                prev_y = y_norm_int
+                prev_z = z_norm_int
+                delta_sock.recv(len(command))
+                i = 1
+            if sqrt( abs(prev_x - x_norm_int) ** 2 + abs(prev_y - y_norm_int)**2 + abs(prev_z - z_norm_int)**2 ) > 300:
+                delta_sock.send(command.encode())
+                prev_x = x_norm_int
+                prev_y = y_norm_int
+                prev_z = z_norm_int
+                delta_sock.recv(len(command))
+            else:
+                continue
+
+
+            # delta_sock.send("G_P".encode())
+            # recv = delta_sock.recv(23).decode()
+            # x_curr, y_curr, curr_z = get_coordinates(recv)
+            #
+            # dx = x_norm_int - x_curr
+            # dy = y_norm_int - y_curr
+            #
+            # norm = sqrt(dx ** 2 + dy ** 2)
+            #
+            # try:
+            #     dx_norm = str(abs(int(dx))).zfill(4)
+            #     dy_norm = str(abs(int(dy))).zfill(4)
+            # except ZeroDivisionError:
+            #     continue
+            #
+            # if dx >= 0:
+            #     dx_str = "+" + dx_norm
+            # else:
+            #     dx_str = "-" + dx_norm
+            #
+            # if dy >= 0:
+            #     dy_str = "+" + dy_norm
+            # else:
+            #     dy_str = "-" + dy_norm
+            #
+            # command = f"JOG{dx_str}{dy_str}+0000TOOL_"
+            # print(command)
+            # delta_sock.send(command.encode())
+            # delta_sock.recv(26)
+
+        sleep(0.25)
+
+
+def execute_command(command):
+    global delta_sock
+    return_value = None
+    prefix = command[0:3]
+
+    if prefix == "G_P":
+        delta_sock.send("G_P".encode())
+        recv = delta_sock.recv(23).decode()
+        return recv
+
+    elif prefix == "LIN":
+        print("Performing: ", command)
+        set_x, set_y, set_z = get_coordinates(command)
+        delta_sock.send(command.encode())
+        sleep(0.5)
+        while True:
+            delta_sock.recv(28)  # clear buffer
+            delta_sock.send("G_P".encode())
+            sleep(0.3)
+            recv = delta_sock.recv(23).decode()
+            curr_x, curr_y, curr_z = get_coordinates(recv)
+            print("Current position: ", curr_x, " ", curr_y, " ", curr_z, " ")
+
+            offsets = [abs(set_x - curr_x), abs(set_y - curr_y), abs(set_z - curr_z)]
+
+            # todo consider checking for moving
+            if max(offsets) <= offset_threshold:
+                break
+    elif prefix == "TIM":
+        timeout = command[3:6]
+        sleep(int(timeout) // 1000)
+
+    elif prefix == "JNT":
+        pass
+
+    elif prefix == "CIR":
+        pass
+
+    sleep(sleep_time)
+    return
+
+
+def get_coordinates(s):
+    x = int(s[4:8]) if s[3] == "+" else int(s[3:8])
+    y = int(s[9:13]) if s[8] == "+" else int(s[8:13])
+    z = int(s[14:18]) if s[13] == "+" else int(s[13:18])
+    return x, y, z
+
+
+"""
+    Program thread and loops
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pd_m", default="models/palm_detection_6_shaves.blob", type=str,
+                    help="Path to a blob file for palm detection model (default=%(default)s)")
+parser.add_argument("--lm_m", default="models/hand_landmark_6_shaves.blob", type=str,
+                    help="Path to a blob file for landmark model (default=%(default)s)")
+args = parser.parse_args()
+
+""" Delta communcation part"""
+
+# variables used to
+delta_host, delta_port = HOST, PORT = "192.168.0.155", 10
+home_pos = "-2000-2000-4500"
+hover_height = "-4000"
+offset_threshold = 100  # how much can differ the real and set position
+sleep_time = 1  # how long in seconds will the G_P command be send while checking if achieved position
+calibration_box_size = (6000, 6000)  # size of the square that is used when calibrating delta vision system
+
+delta_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    delta_sock.connect((delta_host, delta_port))
+    print("Connected with robot delta")
+except Exception as e:
+    print(e)
+
+th1 = threading.Thread(target=delta_loop)
+th1.start()
+#
+ht = HandTracker(pd_path=args.pd_m, lm_path=args.lm_m)
+ht.run()
+#
+th1.join()
